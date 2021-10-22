f8b7fc40214d1e10977e4639f598f5276a571aea
Ashk <timed2a@hotmail.fr>
2021-10-22 10:44:59 +0200

    :building_construction: Example of crud view with posts

---
+++ resources/views/pages/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <x-container.boxed>
+        <h1>Posts - Index</h1>
+    </x-container.boxed>
+@endsection
---
+++ resources/views/pages/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('content')
+    <x-container.boxed>
+        <h1>Posts - Show</h1>
+    </x-container.boxed>
+@endsection
