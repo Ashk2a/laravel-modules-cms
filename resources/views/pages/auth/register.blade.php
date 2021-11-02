be00304a7c62b0fc32fd017a49a9964f5475959b
Ashk <timed2a@hotmail.fr>
2021-11-02 23:03:34 +0100

    :lipstick: Discord button for oauth2

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <x-section.auth :xl="true">
-        <h1 class="text-2xl text-white font-bold text-center mb-10">@lang('global.create_an_account')</h1>
+        <h1 class="text-2xl text-white font-bold text-center mb-5">@lang('global.create_an_account')</h1>
 
         <x-form class="grid grid-cols-2 gap-x-4" method="POST" :action="route('auth.register')">
             <x-form.group class="col-span-2">
@@ -24,9 +24,5 @@
                 <x-form.submit class="btn-base btn-lightBlue w-full justify-center">@lang('global.register')</x-form.submit>
             </x-form.group>
         </x-form>
-
-        <div class="mt-4 text-right text-sm italic text-gold-400 hover:text-gold-600">
-            <a href="{{ route('auth.login') }}">Already have an account ?</a>
-        </div>
     </x-section.auth>
 @endsection
