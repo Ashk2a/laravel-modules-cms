61e32dbd7ee2edbe8abeff56bb0ef8e6638e41e0
Ashk <timed2a@hotmail.fr>
2021-11-06 00:07:30 +0100

    :heavy_minus_sign: VueJs cannot be used with alpine, we keep alpine !

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,4 @@
-import {createApp} from 'vue'
 import Alpine from "alpinejs"
-
-const app = createApp({});
-app
-    .mount('#app');
 
 window.Alpine = Alpine
 Alpine.start()
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,6 @@ const tailwindcss = require('tailwindcss')
 
 mix
     .js("resources/js/app.js", "public/js")
-    .vue({version: 3})
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
