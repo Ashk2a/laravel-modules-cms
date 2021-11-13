639149a2c0c6b9763f61e4074defdd91f07da713
Ashk <timed2a@hotmail.fr>
2021-11-13 02:06:11 +0100

    :lipstick: Add cross on mobile menu to close it when width is too tight

--- modules/Ui/Resources/views/partials/header/mobile/side.blade.php
+++ modules/Ui/Resources/views/partials/header/mobile/side.blade.php
@@ -8,5 +8,11 @@
     @click.away="mobileMenu = false"
     class="lg:hidden overflow-y-auto fixed z-50 w-[24rem] mx-auto p-5 top-0 bottom-0 right-auto bg-blueGray-800 left-0 border-l-2 border-brown-600"
 >
+    <div class="flex justify-end mb-4">
+        <button>
+            <x-heroicon-o-x class="h-6 w-6 text-white" @click.prevent="mobileMenu = false"/>
+        </button>
+    </div>
+
     <x-ui::menu.mobile.container :roots="$roots"/>
 </div>
