b2f5c53417e0d481067f29eb20a885f41648bd33
Ashk <timed2a@hotmail.fr>
2021-11-20 21:30:25 +0100

    :lipstick: Reformat class style

--- modules/Ui/Resources/views/components/header/menu/root.blade.php
+++ modules/Ui/Resources/views/components/header/menu/root.blade.php
@@ -3,17 +3,13 @@
         @mouseover="open = {{ $root->id }}"
         @mouseleave="open = false"
         :class="{'border-gold-500 bg-gradient-to-b from-brown-600-03a to-brown-600-80a text-gray-50' : open === {{ $root->id }} }"
-        class="group flex justify-center items-center w-full
-            border-transparent border-b-2"
+        class="group flex justify-center items-center w-full border-transparent border-b-2"
     >
         <a
             href="{{ $root->getUrl() }}"
             @if (false === $root->categories->isEmpty()) @click.prevent="" @endif
             :class="{'text-gray-50' : open === {{ $root->id }}, 'text-transparent bg-clip-text bg-gradient-to-b from-gold-500 to-gold-50' : open !== {{ $root->id }} }"
-            class="flex inline-flex justify-center items-center
-                w-full py-[1.5rem]
-                text-sm xl:text-lg font-medium
-                group-hover:text-gray-50"
+            class="flex inline-flex justify-center items-center w-full py-[1.5rem] text-sm xl:text-lg font-medium group-hover:text-gray-50"
         >
             <span>{{ $root->name }}</span>
 
@@ -34,9 +30,7 @@
             @mouseover="open = {{ $root->id }}"
             @mouseleave="open = false"
             x-show="open === {{ $root->id }}"
-            class="absolute xl:top-[4.80rem] top-[4.3rem] p-6 w-full
-                grid grid-cols-2 xl:gap-x-10 gap-x-6
-                bg-gradient-to-b from-brown-600-95a to-brown-600"
+            class="absolute xl:top-[4.80rem] top-[4.3rem] p-6 w-full grid grid-cols-2 xl:gap-x-10 gap-x-6 bg-gradient-to-b from-brown-600-95a to-brown-600"
         >
             @foreach($root->categories as $category)
                 @if($category->canShowed())
