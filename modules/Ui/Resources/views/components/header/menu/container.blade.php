2d296bf637439c87a7f0bbd5b964d42676ec6a8a
Ashk <timed2a@hotmail.fr>
2021-11-20 21:28:54 +0100

    :lipstick: Remove menu.scss for the moment, improve in the future

--- modules/Ui/Resources/views/components/header/menu/container.blade.php
+++ modules/Ui/Resources/views/components/header/menu/container.blade.php
@@ -1,4 +1,4 @@
-<div class="menu-side" :roots="[]" x-data="{ open: false }">
+<div class="relative hidden lg:flex justify-between items-center" :roots="[]" x-data="{ open: false }">
     @foreach($roots as $root)
         <x-ui::header.menu.root :root="$root"/>
     @endforeach
--- modules/Ui/Resources/views/components/header/menu/root.blade.php
+++ modules/Ui/Resources/views/components/header/menu/root.blade.php
@@ -2,21 +2,25 @@
     <div
         @mouseover="open = {{ $root->id }}"
         @mouseleave="open = false"
-        :class="{'menu-root-open' : open === {{ $root->id }} }"
-        class="menu-root group"
+        :class="{'border-gold-500 bg-gradient-to-b from-brown-600-03a to-brown-600-80a text-gray-50' : open === {{ $root->id }} }"
+        class="group flex justify-center items-center w-full
+            border-transparent border-b-2"
     >
         <a
             href="{{ $root->getUrl() }}"
             @if (false === $root->categories->isEmpty()) @click.prevent="" @endif
             :class="{'text-gray-50' : open === {{ $root->id }}, 'text-transparent bg-clip-text bg-gradient-to-b from-gold-500 to-gold-50' : open !== {{ $root->id }} }"
-            class="menu-root-item"
+            class="flex inline-flex justify-center items-center
+                w-full py-[1.5rem]
+                text-sm xl:text-lg font-medium
+                group-hover:text-gray-50"
         >
             <span>{{ $root->name }}</span>
 
             @if (false === $root->categories->isEmpty())
                 <x-ui::heroicon
                     x-bind:class="{'text-gray-50' : open === {{ $root->id }}, 'text-gold-300' : open !== {{ $root->id }} }"
-                    class="menu-root-icon"
+                    class="h-4 w-4 ml-2 group-hover:text-gray-50"
                 >
                     M19 9l-7 7-7-7
                 </x-ui::heroicon>
@@ -30,7 +34,9 @@
             @mouseover="open = {{ $root->id }}"
             @mouseleave="open = false"
             x-show="open === {{ $root->id }}"
-            class="menu-dropdown"
+            class="absolute xl:top-[4.80rem] top-[4.3rem] p-6 w-full
+                grid grid-cols-2 xl:gap-x-10 gap-x-6
+                bg-gradient-to-b from-brown-600-95a to-brown-600"
         >
             @foreach($root->categories as $category)
                 @if($category->canShowed())
